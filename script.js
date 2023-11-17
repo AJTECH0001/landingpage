9693dbe0c6c1a034aa33dd9f9fd833436d0ce0ac
AJTECH <aladejamiudamilola>
2023-11-17 10:47:46 +0100

    landingPage

---
+++ script.js
@@ -0,0 +1,27 @@
+const navTogglerButton = document.querySelector('#navToggler')
+
+const openNavIcon = document.querySelector('#openNav')
+
+const closeNavIcon = document.querySelector("#closeNav")
+
+const mainNavWidget = document.querySelector("#mainNav")
+
+navTogglerButton.addEventListener('click', toggleMenu)
+
+
+function toggleMenu() {
+    openNavIcon.classList.toggle('hidden')
+    closeNavIcon.classList.toggle('hidden')
+    mainNavWidget.classList.toggle('hidden')
+}
+
+
+const scrollToTopButton = document.querySelector('#scrollToTop')
+
+
+scrollToTopButton.addEventListener('click', function () {
+    window.scrollTo({
+        top: "0", behavior: 'smooth'
+    })
+})
+
